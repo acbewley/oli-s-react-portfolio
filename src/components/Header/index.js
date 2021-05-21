6300c2ec9cd96122f025dccdce7a1c905826fa7f
acbewley <acbewley13@gmail.com>
2021-05-21 13:44:29 -0400

    pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,9 +9,9 @@ const Header = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/#/">Home</Nav.Link>
-          <Nav.Link href="/#/projects">Portfolio</Nav.Link>
-          <Nav.Link href="/#/contact">Contact</Nav.Link>
+          <Nav.Link href="#/">Home</Nav.Link>
+          <Nav.Link href="#/projects">Portfolio</Nav.Link>
+          <Nav.Link href="#/contact">Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
