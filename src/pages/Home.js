dd17a87a82c88fa4652422f307a6fce374007d63
acbewley <acbewley13@gmail.com>
2021-06-13 09:36:40 -0400

    edited bio

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import img from '../images/me.jpeg'
 import {Jumbo, JumboContent, JumboText, Img} from '../components/Jumbo';
 
-const text = `Hello! My name is Oliver Bewley. I am a coder from the Atlanta area. I've recently graduated from the Georgia Tech Coding Bootcamp on May 27th, 2021. I am well versed in HTML, CSS, JavaScript, React, MySql, and Mongo. My hobbies include; writing and listening to music, knitting, hiking, and playing exclusivly Monster Hunter: Rise.`
+const text = `Hello! My name is Oliver Bewley. I am a coder from the Atlanta area. I've recently graduated from the Georgia Tech Web Development Program on May 27th, 2021. I am well versed in HTML, CSS, JavaScript, React, MySql, and Mongo. My hobbies include; writing and listening to music, knitting, hiking, and playing exclusivly Monster Hunter: Rise.`
 
 function Home() {
   return (
